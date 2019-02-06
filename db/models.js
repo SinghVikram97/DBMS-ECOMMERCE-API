2d67410e01837ea45a4ad4906795d2850608087d
Vikram Singh Bedi <vikram.bedi97@gmail.com>
2019-02-07 00:59:03 +0530

    Added database models

--- db/models.js
+++ db/models.js
@@ -9,6 +9,31 @@ const db = new Sequelize(
   }
 );
 
+const User = db.define("user", {
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  firstName: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING(20),
+    allowNull: false
+  },
+  phone: {
+    type: Sequelize.BIGINT
+  }
+});
+
 const Product = db.define("product", {
   product_id: {
     type: Sequelize.INTEGER,
@@ -44,5 +69,94 @@ const Product = db.define("product", {
       model: Seller,
       key: "seller_id"
     }
+  }
+});
+
+const Category = db.define("category", {
+  category_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING(50)
+  }
+});
+
+const Sellor = db.define("sellor", {
+  sellor_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING(50)
+  },
+  phone: {
+    type: Sequelize.BIGINT
+  },
+  address: {
+    type: Sequelize.STRING(200)
+  },
+  rating: {
+    type: Sequelize.INTEGER
+  }
+});
+
+const Cart = db.define("cart", {
+  cart_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  qty: {
+    type: Sequelize.INTEGER,
+    defaultValue: 0
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: user,
+      key: "user_id"
+    }
+  },
+  productId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: "product",
+      key: product_id
+    }
+  }
+});
+
+const Order = db.define("order", {
+  order_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    unique: true,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: "user",
+      key: user_id
+    }
+  },
+  orderDate: {
+    type: Sequelize.DATE
+  },
+  deliveryDate: {
+    type: Sequelize.DATE
+  },
+  totalPrice: {
+    type: Sequelize.FLOAT
   }
 });
